aea741dc36874a2a7cddbd359c634874664b4e9f
wonjinsong <msmk22239@gmail.com>
2020-07-14 23:11:04 -0400

    4.progress bar

--- js/wall3d.js
+++ js/wall3d.js
@@ -2,6 +2,8 @@
     //전역변수 사용을 피하기 위해
     //house를 움직이자
     const houseElem = document.querySelector('.house');
+    const barElem = document.querySelector('.progress-bar');
+   
     
     //전체문서 높이 - 창높이 = 스크롤할수 있는 높이
     let maxScrollValue;
@@ -19,11 +21,18 @@
         //house를 translateZ로 -490해줘서 hello벽이 뒤로 물러나 있었고,
         //스크롤했을때, 0부터 시작이 아닌, -490빼주고 시작하도록
         //0~1의 비율값에 1000를 곱해 0~1000까지
-        const zMove = pageYOffset/maxScrollValue * 980 - 490;        
+        const scrollPer = pageYOffset/maxScrollValue;
+        const zMove = scrollPer * 980 - 490;        
         houseElem.style.transform = 'translateZ(' + zMove + 'vw)';
+
+        //progress bar
+        barElem.style.width = scrollPer * 100 + '%';
     });
+
+   
 
     //창사이즈가 달라질때마다 resizeHandler호출
     window.addEventListener('resize', resizeHandler);
+    //최초 한번은 호출
     resizeHandler();
 })();
